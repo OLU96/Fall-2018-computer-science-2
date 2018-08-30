76109500977842541c94fea5611e441eacebc7d2
Olumide Longe <LONGE.OLUMIDE@YAHOO.COM>
2018-08-30 10:14:03 -0500

    Add files via upload

---
+++ nameprintout.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string firstname;
+	string lastname;
+	cout << " Please Enter name: " << endl;
+	cin >> firstname >> lastname ;
+	cout << firstname << ", " << lastname << endl;
+	
+	return 0;
+
+}
