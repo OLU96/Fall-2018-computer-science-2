2c89e24a98b4639dec28fa650f07b8883fdd154d
Olumide Longe <LONGE.OLUMIDE@YAHOO.COM>
2018-08-30 10:17:22 -0500

    Add files via upload

---
+++ nameprintout 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << " Please Enter name: " << endl;
+	getline(cin, name);
+	cout << name << endl;
+	
+	return 0;
+
+}
